fd3207bfff2e5260c9caa8d0300fb78608cf1eb2
LinhLuu <linh.luu@axonactive.vn>
2016-04-21 16:44:39 +0700

    upload

--- OptImageEditor/OptImageEditor.h
+++ OptImageEditor/OptImageEditor.h
@@ -16,5 +16,3 @@ FOUNDATION_EXPORT const unsigned char OptImageEditorVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <OptImageEditor/PublicHeader.h>
 
-
-#import <OptImageEditor/CLImageEditor.h>
